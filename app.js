5c4932aefee890c6df3742a47fcfdd669e9ab26c
kite418 <kylembouvier@gmail.com>
2021-11-05 15:12:33 -0400

    milestone2 inital

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import chatInit from './src/chat.js'
 import rtmpInit from './src/rtmp.js';
 import express from 'express';
 import { fileURLToPath } from 'url';
+import bodyParser from "body-parser";
 
 /*
 express/webserver stuff
@@ -33,27 +34,29 @@ function fakeSport() {
    }
 }
 
-
 const app = express();
 const server = Http.createServer(app);
 const httpPort = 3000;
 const __filename = fileURLToPath(import.meta.url);
 app.use(express.static('public'))
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 //user api
-app.get('/user/new', (req, res) => {
+app.get('/signup', (req, res) => {
     res.sendFile(Path.join(__filename, '../public/views/signup.html'));
 });
 
-app.post('/user/new', (req, res) => {
+app.post('/signup', (req, res) => {
     //??? what does this return???
 });
 
-app.get('/user/auth', (req, res) => {
+app.get('/login', (req, res) => {
     res.sendFile(Path.join(__filename, '../public/views/login.html'));
 });
 
-app.post('/usrer/auth', (req, res) => {
+app.post('/login', (req, res) => {
     //??? what does this return???
 })
 
@@ -61,14 +64,26 @@ app.get('/user/info', (req, res) => {
     let fakeRes = {
         username: faker.name.firstName(),
         stream_key: faker.lorem.words(),
+        stream_title: faker.lorem.words(),
+        stream_category: faker.lerem.words(),
     }
 
     res.json(fakeRes);
 });
 
+app.get('/user', (req, res) => {
+    res.sendFile(Path.join(__filename, '../public/views/user.html'));
+})
+
 //stream api
 app.post('/stream/update', (req, res) => {
-    res.sendStatus(200);
+    console.log(req.body.title)
+    console.log(req.body.category)
+
+    //verify title and category
+    res.json({
+        success: true //or false if failed
+    });
 });
 
 app.get('/stream/categories', (req, res) => {
@@ -82,6 +97,10 @@ app.get('/stream/categories', (req, res) => {
 
     res.json(fakeRes);
 });
+
+app.get('/stream/browse', (req, res) => {
+    res.sendFile(Path.join(__filename, '../public/views/category.html'));
+})
 
 //sports api
 app.get('/sports/get', (req, res) => {
@@ -104,11 +123,6 @@ app.get('/sports', (req, res) => {
 app.get('/live/:username', (req, res) => {
     res.sendFile(Path.join(__filename, '../public/views/stream.html'));
 });
-
-//other
-app.get('/categories/:category', (req, res) => {
-    res.sendFile(Path.join(__filename, '../public/views/category.html'));
-})
 
 //setup chat
 chatInit();
