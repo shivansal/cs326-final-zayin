7d6fee6bb8ba627a6dd5f65a50263c78ba76f869
shivansal <ansalshiv@gmail.com>
2021-11-06 15:50:41 -0400

    sign up page

---
+++ public/js/signup.js
@@ -0,0 +1,37 @@
+let username = document.getElementById('username');
+let password = document.getElementById('password');
+let confirmPassword = document.getElementById('confirm-password');
+let signupBtn = document.getElementById('signup-btn');
+let statusMsg = document.getElementById('status-msg');
+
+function handleResponse(response) {
+    statusMsg.className = '';
+    if (response.success) {
+        //perform redirect
+        window.location.replace(response.redirectUrl);
+    } else {
+        //display the error message
+        statusMsg.innerText = 'Error: ' + response.error 
+        statusMsg.classList.add('failed');
+    }
+}
+
+signupBtn.addEventListener('click', function() {
+    //do clientside validation
+
+    if(password != confirmPassword){
+        statusMsg.innerText = "Error: passwords don't match"
+        statusMsg.classList.add('failed');
+    }
+    else{
+        let body = JSON.stringify({
+            username: username.innerText,
+            password: password.innerText,
+        });
+    
+        fetch('http://localhost:3000/signup', {method: "POST", body: body})
+        .then(response => {
+            return response.json()
+        }).then(handleResponse)
+    }   
+})
