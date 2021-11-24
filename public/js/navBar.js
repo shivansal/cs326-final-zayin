3fc47d53b0ec7b2c49dc95bce509a07e8f24e411
beepbeepbop <mihirthakur@umass.edu>
2021-11-23 19:41:20 -0500

    display_profile_picture

--- public/js/navBar.js
+++ public/js/navBar.js
@@ -1,6 +1,7 @@
 getUserInfo();
 loadBrowseDropDown();
 const userBox = document.getElementById("userInfo");
+const profilePic = document.getElementById("profilePic");
 
 async function getUserInfo() {
     //https://cs326-zayin.herokuapp.com/user/info
@@ -14,20 +15,23 @@ async function getUserInfo() {
     let user = await response.json();
     if (user.success) {
         userBox.innerHTML = "@" + user.username;
+        profilePic.setAttribute("src", user.profilepic);
+        profilePic.setAttribute("height", "50px");
+        profilePic.setAttribute("width", "50px");
         document.getElementById('signin-btn').remove();
         document.getElementById('userInfo').classList.remove('hidden');
     } else {
         userBox.innerHtml = '';
     }
 
-    
-    
+
+
 }
 
 async function loadBrowseDropDown() {
     //<a class="dropdown-item" href="http://localhost:3000/user"></a></a>
     let browseDropDown = document.getElementById('browse-dropdown');
-    
+
 
     const response = await fetch(window.URL_BASE + "/sports/get", {
         headers: {
